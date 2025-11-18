3f11db2fcdd485bbfe400b62994440515810b100
Ailton Luiz Dias Siqueira Junior <ailtonljr@yahoo.com.br>
2025-11-18 11:31:17 -0300

    Refactor LDR sensor setup and reading logic for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,40 @@
 #include <Arduino.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
 
-// Use A2 para garantir que estamos falando da porta Analógica
-const int pinoEnergiaLDR = 3; // Ligue o VCC do sensor na Digital 3
-const int pinoLeituraLDR = A2; // Ligue o A0 do sensor na Analógica A2
+const int pinoEnergiaLDR = 3;
+const int pinoLeituraLDR = A2;
+
+byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+
+const char* mqttServer = "broker.hivemq.com";
+const int mqttPort = 1883;
+
+// EthernetClient ethClient;
+// PubSubClient client(ethClient);
 
 void setup() {
-  // Garanta que o monitor_speed no platformio.ini também é 9600
   Serial.begin(9600);
-  
-  pinMode(6, OUTPUT);
-  digitalWrite(6, LOW);
-  Serial.println("--- INICIANDO O ARDUINO ---"); // Mensagem de teste
-
-  // Configura a alimentação "gambiarra" via pino digital
   pinMode(pinoEnergiaLDR, OUTPUT);
-  digitalWrite(pinoEnergiaLDR, HIGH); // Liga 5V no sensor
-  
-  Serial.println("Sensor alimentado! Entrando no loop...");
+  digitalWrite(pinoEnergiaLDR, HIGH);
+  Serial.println("Lendo valor do LDR...");
+  // Ethernet.begin(mac);
+  // client.setServer(mqttServer, mqttPort);
 }
 
-void loop() {
-  // Lê o valor analógico (0 a 1023)
-  int valorLDR = analogRead(pinoLeituraLDR);
-  
-  Serial.print("Luz: ");
-  Serial.print(valorLDR);
-  
-  // Interpretação básica para te ajudar
-  if(valorLDR < 200) {
-    Serial.println(" (Escuro / Noite)");
-  } else if(valorLDR > 800) {
-    Serial.println(" (Muito Claro / Sol)");
-  } else {
-    Serial.println(" (Meia luz)");
-  }
+// void reconect(){
+//   while (!client.connected()) {
+//     if (client.connect("ArduinoClient")) {
+//     } else {
+//       delay(2000);
+//     }
+//   }
+// }
 
-  delay(1000);
+void loop() {
+  
+  int valorLDR = analogRead(pinoLeituraLDR);
+  Serial.print("Valor do LDR: ");
+  Serial.println(valorLDR);
+  delay(2000);
 }
